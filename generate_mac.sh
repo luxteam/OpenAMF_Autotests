b15874f9c66a588e68d20aef01fed97918f1a0af
Piromancer <Den2000_09@mail.ru>
2020-08-17 15:45:13 +0300

    Path changed to OpenCL-Headers submodule in shell script

--- generate_mac.sh
+++ generate_mac.sh
@@ -6,6 +6,6 @@ rm -rf ./build
 mkdir build
 cd build
 
-cmake .. -DOpenCL_INCLUDE_DIR="../../../../../../CL_Headers" -DAMF_CORE_STATIC="true"
+cmake .. -DOpenCL_INCLUDE_DIR="../../../../../OpenCL-Headers" -DAMF_CORE_STATIC="true"
 
 cd ..
