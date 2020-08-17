2fee23a008cd8217e7365a23a59c973ae1ac12ab
Piromancer <Den2000_09@mail.ru>
2020-08-17 16:54:45 +0300

    Changes from master merged

---
+++ generate-mac-shared.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+
+clear
+
+rm -rf ./build
+mkdir build
+cd build
+
+cmake .. -DOpenCL_INCLUDE_DIR="../../../../../Thirdparty/OpenCL-Headers" -DAMF_CORE_SHARED="true"
+
+cd ..
---
+++ generate-xcode-static.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+clear
+
+rm -rf ./build
+mkdir build
+cd build
+
+cmake -G "Xcode" .. -DOpenCL_INCLUDE_DIR="../../../../../Thirdparty/OpenCL-Headers" -DAMF_CORE_STATIC="true"
+
+cd ..
