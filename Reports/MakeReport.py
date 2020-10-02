4c5374b78005a5e78f8b6dda290ef591845edbbc
Piromancer <Den2000_09@mail.ru>
2020-10-02 15:50:47 +0300

    Added example .json and completed templates

--- Reports/MakeReport.py
+++ Reports/MakeReport.py
@@ -1,16 +1,20 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import sys
 import json
+import os
+
+if not os.path.exists('report'):
+    os.mkdir('report')
 
 # Reading from all .json files with results from all GPUs (passed as arguments)
 statistics = list()
 for i in range(1, len(sys.argv)):
     results_json = sys.argv[i]
-    with open(results_json, "r") as results_file:
+    with open(results_json, 'r') as results_file:
         statistics.append(json.load(results_file))
 
 env = Environment(
-    loader=FileSystemLoader('templates/')
+    loader=FileSystemLoader('templates')
 )
 
 ### Creating main page ###    
@@ -21,8 +25,8 @@ gpu_stat = list()
 for result in statistics:
     gpu_stat.append(
         {
-        'GPU' : 'Radeon VII',
-        'Configuration' : 'Static VS2017',
+        'GPU' : 'GPU_NAME-OS',
+        'Configuration' : 'CONFIGURATION',
         'Total' : result['tests'],
         'Passed' : result['tests'] - result['failures'] - result['disabled'] - result['errors'],
         'Failed' : result['failures'],
@@ -34,9 +38,40 @@ for result in statistics:
     )
 out_file = template.render(gpu_stat=gpu_stat)
 
-with open("templates/realMainPage.html", "w") as fh:
+with open('report/realMainPage.html', 'w') as fh:
     fh.write(out_file)
 
 
 # Forming information for 'Details' sections
 template = env.get_template('test_suite_details_page.html')
+for stat in statistics:
+    for result in stat['testsuites']:
+
+        for test_case in result['testsuite']:
+            if 'DISABLED_' in test_case['name']:
+                test_case['Result'] = 'Skipped'
+            elif not 'failures' in test_case:
+                test_case['Result'] = 'Passed'
+            else:
+                test_case['Result'] = 'Failure<br>'
+                for fail in test_case['failures']:
+                    test_case['Result'] += fail['failure']
+
+        test_suite = {
+            'GPU' : 'GPU_NAME-OS',
+            'Configuration' : 'CONFIGURATION',
+            'Name' : result["name"],
+            'Total' : result['tests'],
+            'Passed' : result['tests'] - result['failures'] - result['disabled'] - result['errors'],
+            'Failed' : result['failures'],
+            'Disabled' : result['disabled'],
+            'Error' : result['errors'],
+            'Time taken': result['time'],
+            'Time stamp': result['timestamp'].replace('T', ' ').replace('Z', ''),
+            'Test cases': result['testsuite']
+            }
+        
+        out_file = template.render(test_suite=test_suite)
+
+        with open(os.path.join('report', 'details_' + result['name'] + "_" + "GPU-OS" + '.html'), 'w') as fh:
+            fh.write(out_file)
