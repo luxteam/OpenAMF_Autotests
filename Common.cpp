ace95ac0afa3293c091787823fcaf767e1f6c517
Piromancer <Den2000_09@mail.ru>
2020-11-23 17:15:26 +0300

    Covered AMFSTL, working on Threads

--- Common.cpp
+++ Common.cpp
@@ -23,7 +23,7 @@ struct AMF_Mutex : testing::Test {
 };
 
 void printThread() {
-	AMFMutex mutex1 = amf_open_mutex(L"\global1");
+	AMFMutex mutex1 = amf_open_mutex(L"\\global1");
 	mutex1.Lock();
 	for (int i = 0; i < 10000; i++)
 		std::cout << "thread function Executing" << std::endl;
@@ -31,13 +31,106 @@ void printThread() {
 }
 
 TEST_F(AMF_Mutex, AMFMutex1) {
-	AMFMutex mutex1 = amf_create_mutex(false, L"\global1");
+	AMFMutex mutex1 = amf_create_mutex(false, L"\\global1");
 	threadObj = thread(printThread);
 	mutex1.Lock();
 	for (int i = 0; i < 10000; i++)
 		std::cout << "Display From MainThread" << std::endl;
 	mutex1.Unlock();
 	ASSERT_TRUE(false);
+}
+
+TEST_F(AMF_Mutex, amf_atomic_inc) {
+}
+
+TEST_F(AMF_Mutex, amf_atomic_dec) {
+}
+
+TEST_F(AMF_Mutex, amf_create_critical_section) {
+}
+
+TEST_F(AMF_Mutex, amf_delete_critical_section) {
+}
+
+TEST_F(AMF_Mutex, amf_enter_critical_section) {
+}
+
+TEST_F(AMF_Mutex, amf_leave_critical_section) {
+}
+
+TEST_F(AMF_Mutex, amf_create_event) {
+}
+
+TEST_F(AMF_Mutex, amf_delete_event) {
+}
+
+TEST_F(AMF_Mutex, amf_set_event) {
+}
+
+TEST_F(AMF_Mutex, amf_reset_event) {
+}
+
+TEST_F(AMF_Mutex, amf_wait_for_event) {
+}
+
+TEST_F(AMF_Mutex, amf_wait_for_event_timeout) {
+}
+
+TEST_F(AMF_Mutex, amf_create_mutex) {
+}
+
+TEST_F(AMF_Mutex, amf_open_mutex) {
+}
+
+TEST_F(AMF_Mutex, amf_delete_mutex) {
+}
+
+TEST_F(AMF_Mutex, amf_wait_for_mutex) {
+}
+
+TEST_F(AMF_Mutex, amf_release_mutex) {
+}
+
+TEST_F(AMF_Mutex, amf_create_semaphore) {
+}
+
+TEST_F(AMF_Mutex, amf_delete_semaphore) {
+}
+
+TEST_F(AMF_Mutex, amf_wait_for_semaphore) {
+}
+
+TEST_F(AMF_Mutex, amf_release_semaphore) {
+}
+
+TEST_F(AMF_Mutex, amf_sleep) {
+}
+
+TEST_F(AMF_Mutex, amf_high_precision_clock) {
+}
+
+TEST_F(AMF_Mutex, amf_increase_timer_precision) {
+}
+
+TEST_F(AMF_Mutex, amf_restore_timer_precision) {
+}
+
+TEST_F(AMF_Mutex, amf_load_library) {
+}
+
+TEST_F(AMF_Mutex, amf_get_proc_address) {
+}
+
+TEST_F(AMF_Mutex, amf_free_library) {
+}
+
+TEST_F(AMF_Mutex, get_current_thread_id) {
+}
+
+TEST_F(AMF_Mutex, amf_virtual_alloc) {
+}
+
+TEST_F(AMF_Mutex, amf_virtual_free) {
 }
 
 struct AMF_STL : testing::Test {
@@ -68,4 +161,105 @@ struct AMF_STL : testing::Test {
 TEST_F(AMF_STL, amf_from_unicode_to_utf8) {
 	res_str = amf_from_unicode_to_utf8(wstr);
 	EXPECT_EQ(res_str, str);
+}
+
+TEST_F(AMF_STL, amf_from_utf8_to_unicode) {
+	res_wstr = amf_from_utf8_to_unicode(str);
+	EXPECT_EQ(res_wstr, wstr);
+}
+
+TEST_F(AMF_STL, amf_from_unicode_to_multibyte) {
+	res_str = amf_from_unicode_to_multibyte(wstr);
+	EXPECT_EQ(res_str, str);
+}
+
+TEST_F(AMF_STL, amf_from_multibyte_to_unicode) {
+	res_wstr = amf_from_multibyte_to_unicode(str);
+	EXPECT_EQ(res_wstr, wstr);
+}
+
+TEST_F(AMF_STL, amf_from_string_to_hex_string) {
+	res_str = amf_from_string_to_hex_string(str);
+	EXPECT_EQ(res_str.c_str(), str.c_str());
+}
+
+TEST_F(AMF_STL, amf_from_hex_string_to_string) {
+	res_str = amf_from_hex_string_to_string(str);
+	EXPECT_EQ(res_str.c_str(), str.c_str());
+}
+
+TEST_F(AMF_STL, amf_string_to_lower) {
+	const amf_string upper_str = "TEST";
+	res_str = amf_string_to_lower(upper_str);
+	EXPECT_EQ(res_str, str);
+}
+
+TEST_F(AMF_STL, amf_string_to_lower_wstr) {
+	const amf_wstring upper_str = L"TEST";
+	res_wstr = amf_string_to_lower(upper_str);
+	EXPECT_EQ(res_wstr, wstr);
+}
+
+TEST_F(AMF_STL, amf_string_to_upper) {
+	const amf_string upper_str = "TEST";
+	res_str = amf_string_to_upper(str);
+	EXPECT_EQ(res_str, upper_str);
+}
+
+TEST_F(AMF_STL, amf_string_to_upper_wstr) {
+	const amf_wstring upper_str = L"TEST";
+	res_wstr = amf_string_to_upper(wstr);
+	EXPECT_EQ(res_wstr, upper_str);
+}
+
+TEST_F(AMF_STL, amf_from_unicode_to_url_utf8) {
+	res_str = amf_from_unicode_to_url_utf8(wstr);
+	EXPECT_EQ(res_str, str);
+}
+
+TEST_F(AMF_STL, amf_from_url_utf8_to_unicode) {
+	res_wstr = amf_from_url_utf8_to_unicode(str);
+	EXPECT_EQ(res_wstr, wstr);
+}
+
+TEST_F(AMF_STL, amf_convert_path_to_os_accepted_path) {
+	res_wstr = amf_convert_path_to_os_accepted_path(wstr);
+	EXPECT_EQ(res_wstr, wstr);
+}
+
+TEST_F(AMF_STL, amf_convert_path_to_url_accepted_path) {
+	res_wstr = amf_convert_path_to_url_accepted_path(wstr);
+	EXPECT_EQ(res_wstr, wstr);
+}
+
+TEST_F(AMF_STL, amf_string_format) {
+	res_str = amf_string_format("test %s %d", "one", 1);
+	EXPECT_EQ(res_str, "test one 1");
+}
+
+TEST_F(AMF_STL, amf_string_format_wstr) {
+	res_wstr = amf_string_format(L"test %s %d", L"one", 1);
+	EXPECT_EQ(res_wstr, L"test one 1");
+}
+
+TEST_F(AMF_STL, DISABLED_amf_string_formatVA) {
+	//res_str = amf_string_formatVA(str.c_str);
+	EXPECT_EQ(res_str, str);
+}
+
+TEST_F(AMF_STL, DISABLED_amf_string_formatVA_wstr) {
+	res_str = amf_from_unicode_to_utf8(wstr);
+	EXPECT_EQ(res_str, str);
+}
+
+TEST_F(AMF_STL, amf_string_ci_compare) {
+	amf_wstring wstr2 = L"test";
+	amf_int res = amf_string_ci_compare(wstr, wstr2);
+	EXPECT_EQ(res, 0);
+}
+
+TEST_F(AMF_STL, amf_string_ci_rfind) {
+	amf_wstring wstr2 = L"test";
+	amf_int res = amf_string_ci_rfind(wstr, wstr2);
+	EXPECT_EQ(res, 0);
 }
