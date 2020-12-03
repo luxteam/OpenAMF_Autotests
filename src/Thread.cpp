f931ad4cb9b7c3756c0f6f59890243a26de6625e
Piromancer <Den2000_09@mail.ru>
2020-12-03 17:59:12 +0300

    Naming fix

--- src/Thread.cpp
+++ src/Thread.cpp
@@ -8,7 +8,7 @@ static amf_handle event = amf_create_event(false, true, L"test_event");
 static amf_long x = 5;
 
 
-struct AMF_Thread : testing::Test {
+struct Thread : testing::Test {
 	AMF_RESULT res;
 	chrono::time_point<chrono::system_clock> startTime;
 	thread threadObj;
@@ -21,11 +21,11 @@ struct AMF_Thread : testing::Test {
 		terminateTestSuiteLog();
 	}
 
-	AMF_Thread() {
+	Thread() {
 		startTime = initiateTestLog();
 	}
 
-	~AMF_Thread() {
+	~Thread() {
 		terminateTestLog(startTime);
 	}
 };
@@ -38,7 +38,7 @@ void printThread() {
 	mutex1.Unlock();
 }
 
-TEST_F(AMF_Thread, AMFMutex1) {
+TEST_F(Thread, AMFMutex1) {
 	threadObj = thread(printThread);
 	mutex1.Lock();
 	for (int i = 0; i < 10; i++)
@@ -53,7 +53,7 @@ void inc() {
 	amf_atomic_inc(&x);
 }
 
-TEST_F(AMF_Thread, amf_atomic_inc) {
+TEST_F(Thread, amf_atomic_inc) {
 	threadObj = thread(inc);
 	amf_atomic_inc(&x);
 	threadObj.join();
@@ -61,7 +61,7 @@ TEST_F(AMF_Thread, amf_atomic_inc) {
 	x = 5;
 }
 
-TEST_F(AMF_Thread, amf_atomic_dec) {
+TEST_F(Thread, amf_atomic_dec) {
 	threadObj = thread(inc);
 	amf_atomic_dec(&x);
 	threadObj.join();
@@ -110,13 +110,13 @@ void operateArray() {
 	PrintArray();
 }
 
-TEST_F(AMF_Thread, DISABLED_amf_enter_critical_section) {
+TEST_F(Thread, DISABLED_amf_enter_critical_section) {
 	threadObj = thread(operateArray);
 	operateArray();
 	EXPECT_TRUE(false);
 }
 
-TEST_F(AMF_Thread, DISABLED_amf_delete_critical_section) {
+TEST_F(Thread, DISABLED_amf_delete_critical_section) {
 	amf_delete_critical_section(crit_section);
 	EXPECT_ANY_THROW(amf_enter_critical_section(crit_section));
 	crit_section = amf_create_critical_section();
@@ -127,7 +127,7 @@ void eventTest() {
 	x *= 2;
 }
 
-TEST_F(AMF_Thread, amf_set_event) {
+TEST_F(Thread, amf_set_event) {
 	threadObj = thread(eventTest);
 	x++;
 	amf_set_event(event);
@@ -136,19 +136,19 @@ TEST_F(AMF_Thread, amf_set_event) {
 	x = 5;
 }
 
-TEST_F(AMF_Thread, amf_delete_event) {
+TEST_F(Thread, amf_delete_event) {
 	amf_delete_event(event);
 }
 
-TEST_F(AMF_Thread, amf_reset_event) {
+TEST_F(Thread, amf_reset_event) {
 	EXPECT_NO_THROW(amf_reset_event(event));
 }
 
-TEST_F(AMF_Thread, amf_wait_for_event_timeout) {
+TEST_F(Thread, amf_wait_for_event_timeout) {
 	EXPECT_NO_THROW(amf_wait_for_event_timeout(event, 10));
 }
 
-TEST_F(AMF_Thread, DISABLED_amf_open_mutex) {
+TEST_F(Thread, DISABLED_amf_open_mutex) {
 	threadObj = thread(inc);
 	AMFMutex mutex = amf_open_mutex(L"\\global1");
 	mutex.Lock();
@@ -158,55 +158,55 @@ TEST_F(AMF_Thread, DISABLED_amf_open_mutex) {
 	x = 5;
 }
 
-TEST_F(AMF_Thread, amf_delete_mutex) {
+TEST_F(Thread, amf_delete_mutex) {
 	amf_delete_mutex(mutex2);
 	amf_wait_for_mutex(mutex2, 100);
 }
 
-TEST_F(AMF_Thread, amf_release_mutex) {
+TEST_F(Thread, amf_release_mutex) {
 	amf_wait_for_mutex(mutex2, 100);
 	amf_release_mutex(mutex2);
 }
 /*
-TEST_F(AMF_Thread, amf_create_semaphore) {
+TEST_F(Thread, amf_create_semaphore) {
 }
 
-TEST_F(AMF_Thread, amf_delete_semaphore) {
+TEST_F(Thread, amf_delete_semaphore) {
 }
 
-TEST_F(AMF_Thread, amf_wait_for_semaphore) {
+TEST_F(Thread, amf_wait_for_semaphore) {
 }
 
-TEST_F(AMF_Thread, amf_release_semaphore) {
+TEST_F(Thread, amf_release_semaphore) {
 }
 
-TEST_F(AMF_Thread, amf_sleep) {
+TEST_F(Thread, amf_sleep) {
 }
 
-TEST_F(AMF_Thread, amf_high_precision_clock) {
+TEST_F(Thread, amf_high_precision_clock) {
 }
 
-TEST_F(AMF_Thread, amf_increase_timer_precision) {
+TEST_F(Thread, amf_increase_timer_precision) {
 }
 
-TEST_F(AMF_Thread, amf_restore_timer_precision) {
+TEST_F(Thread, amf_restore_timer_precision) {
 }
 
-TEST_F(AMF_Thread, amf_load_library) {
+TEST_F(Thread, amf_load_library) {
 }
 
-TEST_F(AMF_Thread, amf_get_proc_address) {
+TEST_F(Thread, amf_get_proc_address) {
 }
 
-TEST_F(AMF_Thread, amf_free_library) {
+TEST_F(Thread, amf_free_library) {
 }
 
-TEST_F(AMF_Thread, get_current_thread_id) {
+TEST_F(Thread, get_current_thread_id) {
 }
 
-TEST_F(AMF_Thread, amf_virtual_alloc) {
+TEST_F(Thread, amf_virtual_alloc) {
 }
 
-TEST_F(AMF_Thread, amf_virtual_free) {
+TEST_F(Thread, amf_virtual_free) {
 }
 */
