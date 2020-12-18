7db557f09c4e6af07d46fd2a00fd049cf00554f8
Piromancer <Den2000_09@mail.ru>
2020-12-18 16:42:52 +0300

    ENABLE_METAL

--- generate-xcode-static.sh
+++ generate-xcode-static.sh
@@ -5,6 +5,6 @@ rm -rf ./build
 mkdir build
 cd build
 
-cmake -G "Xcode" .. -DOpenCL_INCLUDE_DIR="../../../../../Thirdparty/OpenCL-Headers" -DAMF_CORE_STATIC="true"
+cmake -G "Xcode" .. -DOpenCL_INCLUDE_DIR="../../../../../Thirdparty/OpenCL-Headers" -DENABLE_METAL=1 -DAMF_CORE_STATIC="true"
 
 cd ..
